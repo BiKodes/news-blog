959cd15750a31e766914e26eebdb835ec7b9600c
BikoCodes <bikocodes@gmail.com>
2022-06-17 14:44:25 +0300

    postpage: refactoring the more artciles sections by replacing flex with grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        'screen-mid': '60vh',
+        'screen/2': '82vh',
+        'screen/3': 'calc(100vh / 3)',
+        'screen/4': 'calc(100vh / 4)',
+        'screen/5': 'calc(100vh / 5)',
+      }
+    },
   },
   plugins: [],
 }
